37a224a8dfbd388b7c3e2b5bbbaf49bdf9470504
Brennan Ebeling <brennan@brightspot.com>
2023-09-22 11:56:50 -0400

    Build json for SQS body

--- analysis/send-sqs.sh
+++ analysis/send-sqs.sh
@@ -2,7 +2,21 @@
 
 set -euo pipefail
 
+bsp_version="$(cat "$1")"
+digest="$(jq -r '."containerimage.digest"' "$2")"
+repo="$GITHUB_REPOSITORY"
+build="${GITHUB_REF##*/}"
+commit="$GITHUB_SHA"
+
+json="$(jq -cn \
+  --arg repository "https://github.com/$repo" \
+  --arg build "$build" \
+  --arg commit "$commit" \
+  --arg dockerDigest "$digest" \
+  --arg brightspotVersion "$bsp_version" \
+  '$ARGS.named')"
+
 aws sqs send-message \
-  --queue-url '' \
-  --message-body "$(cat "$1")"
+  --queue-url 'TODO' \
+  --message-body "$json"
 
